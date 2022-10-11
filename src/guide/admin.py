e4eb348d2b9fa63bb0e4aea22438f31b49092692
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2022-10-11 14:56:21 +0300

    to models.py add age restrictions, book's format and binding(2)

--- src/guide/admin.py
+++ src/guide/admin.py
@@ -5,4 +5,7 @@ admin.site.register(models.Author)
 admin.site.register(models.Genre)
 admin.site.register(models.Publishing)
 admin.site.register(models.Series)
+admin.site.register(models.Restrictions)
+admin.site.register(models.Format)
+admin.site.register(models.Binding)
 
