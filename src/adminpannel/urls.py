4bd9d9b89e816a4557fae14a012a3e49c2876183
Maksim Ivanov <ivanov.maksim.blr@gmail.com>
2022-12-12 17:38:27 +0300

    debug comments

--- src/adminpannel/urls.py
+++ src/adminpannel/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views as ap_view
 app_name = 'adminportal'
 urlpatterns = [
- path(" ", ap_view.AdminPannel.as_view(), name="adminpannel"),
- path("book/", ap_view.AdminPannelBook.as_view(), name="book"),
- path("user-profile", ap_view.UserPannel.as_view(), name="userpannel"),
- path("exchangerate/<int:pk>", ap_view.UpdateExchangeRate.as_view(), name="exchangerate")
+    path(" ", ap_view.AdminPannel.as_view(), name="adminpannel"),
+    path("book/", ap_view.AdminPannelBook.as_view(), name="book"),
+    path("user-profile", ap_view.UserPannel.as_view(), name="userpannel"),
+    path("exchangerate/<int:pk>", ap_view.UpdateExchangeRate.as_view(), name="exchangerate")
  
 
 ]
